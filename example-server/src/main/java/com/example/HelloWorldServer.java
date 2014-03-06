83b2b11a2e41fb490f41ce09c3c44653e8acc7b8
Devin Smith <dsmith@redcurrent.com>
2014-03-06 18:09:59 +0000

    added stuff

---
+++ example-server/src/main/java/com/example/HelloWorldServer.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+
+/**
+ *  * @author Stuart Douglas
+ *   */
+@UndertowExample("Hello World")
+public class HelloWorldServer {
+
+    public static void main(final String[] args) {
+        Undertow server = Undertow.builder()
+                .addListener(8080, "localhost")
+                .setHandler(new HttpHandler() {
+                    @Override
+                    public void handleRequest(final HttpServerExchange exchange) throws Exception {
+                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                        exchange.getResponseSender().send("Hello World");
+                    }
+                }).build();
+        server.start();
+    }
+
+}
