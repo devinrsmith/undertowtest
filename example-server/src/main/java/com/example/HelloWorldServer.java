12b33b326bd73d2f8340eabe0aa93b0037fb23df
Devin Smith <dsmith@walleyesoftware.com>
2014-03-06 12:50:36 -0600

    user counter

--- example-server/src/main/java/com/example/HelloWorldServer.java
+++ example-server/src/main/java/com/example/HelloWorldServer.java
@@ -5,19 +5,22 @@ import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  *  * @author Stuart Douglas
  *   */
 public class HelloWorldServer {
 
     public static void main(final String[] args) {
-        Undertow server = Undertow.builder()
+        final AtomicInteger count = new AtomicInteger(1);
+        final Undertow server = Undertow.builder()
                 .addHttpListener(8080, "172.31.26.50")
                 .setHandler(new HttpHandler() {
                     @Override
                     public void handleRequest(final HttpServerExchange exchange) throws Exception {
                         exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/html");
-                        exchange.getResponseSender().send("<h2>Hello redcurrent!</h2>");
+                        exchange.getResponseSender().send("<h2>Hello redcurrent!</h2><h1>Visitor Count: " + count.getAndIncrement() + "</h1>");
                     }
                 }).build();
         server.start();
